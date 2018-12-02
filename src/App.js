e81aab3c8f8a7718e0aaf9dd1aa5c111e8663bd8
Richard May <richard@italist.com>
2018-12-02 09:56:06 -0800

    add country flags

--- src/App.js
+++ src/App.js
@@ -21,16 +21,23 @@ export class App extends Component {
           <Col xs={3}>Winnings</Col>
           <Col xs={3}>Country</Col>
         </Row>
-        {this.state.players.map(({ avatar, country, first, id, last, winnings }) => (
-          <Row key={id}>
+        {this.state.players.map(({ alpha2code, avatar, country, first, id, last, winnings }) => (
+          <Row key={id} middle="xs">
             <Col xs={3}>
-              <img alt={`${first} ${last}}`} src={avatar} />
+              <img alt={`${first} ${last}`} src={avatar} />
             </Col>
             <Col xs={3}>
               {first} {last}
             </Col>
             <Col xs={3}>{winnings}</Col>
-            <Col xs={3}>{country}</Col>
+            <Col xs={3}>
+              <img
+                src="https://res.cloudinary.com/viz/image/upload/flag/blank.gif"
+                className={`flag flag-${alpha2code.toLowerCase()}`}
+                alt=""
+              />
+              {country}
+            </Col>
           </Row>
         ))}
       </div>
